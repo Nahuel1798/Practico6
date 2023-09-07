e9554faf2c7e9d0f7a892d0a968d8db77cc59b92
Nahuel1798 <nahuellucero25@gmail.com>
2023-09-07 12:01:06 -0300

    Initial commit

---
+++ src/main/java/Producto/Producto.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Producto;
+
+/**
+ *
+ * @author Nahue
+ */
+public class Producto implements Comparable<Producto>{
+    private int codigo;
+    private String descripcion;
+    private double precio;
+    private int Stock;
+    private String rubro;
+
+    public Producto(int codigo, String descripcion, double precio, int Stock, String rubro) {
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.Stock = Stock;
+        this.rubro = rubro;
+    }
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return Stock;
+    }
+
+    public void setStock(int Stock) {
+        this.Stock = Stock;
+    }
+
+    public String getRubro() {
+        return rubro;
+    }
+
+    public void setRubro(String rubro) {
+        this.rubro = rubro;
+    }
+
+    @Override
+    public int compareTo(Producto t) {
+        if(codigo==t.codigo){
+            return 0;
+        }else if(codigo > t.codigo){
+            return 1;
+        }else{
+            return -1;
+        }
+    }
+    
+}
